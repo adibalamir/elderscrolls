04da718e141f8888ed047b338ac5af7c61c0e053
Adih <adibalamir@hotmail.com>
2021-04-02 13:21:31 -0400

    Add service to fetch data from elderscrolls api

---
+++ src/app/card.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CardService {
+  constructor(private http: HttpClient) {}
+
+  getCards(page: number): Observable<any> {
+    const cards = this.http.get(
+      `https://api.elderscrollslegends.io/v1/cards?page=${page}&pageSize=10`
+    ) as Observable<any>;
+
+    return cards;
+  }
+}
